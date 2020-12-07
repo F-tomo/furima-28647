e96b060a841e6a2f1b752b927b89003c02090988
F-tomo <tmk.determination714@icloud.com>
2020-12-07 17:53:45 +0900

    Fakerの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,14 +5,12 @@ FactoryBot.define do
     end
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
-    password              { password }
+    password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name             { person.last.kanji }
     first_name            { person.first.kanji }
     last_name_kana        { person.last.katakana }
     first_name_kana       { person.first.katakana }
-    birthday = Faker::Date.birthday
-    birth_date            { birthday }
+    birth_date            { Faker::Date.birthday }
   end
 end
