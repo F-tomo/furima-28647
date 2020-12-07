2f6f0e4b1abf84b72cf0b28651e4084ab028c2c4
F-tomo <tmk.determination714@icloud.com>
2020-12-07 20:03:00 +0900

    バリデーション追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :password,        presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :birth_date
+  end
+  
   with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } do
     validates :last_name
     validates :first_name
     validates :last_name_kana
     validates :first_name_kana
   end
-  validates :birth_date,      presence: true
+  
 end
