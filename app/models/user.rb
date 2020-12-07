a8799fa2624c7ccf5e1c8cf99f05290609befcc0
F-tomo <tmk.determination714@icloud.com>
2020-12-07 17:59:36 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ApplicationRecord
 
   validates :nickname,        presence: true
   validates :password,        presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   validates :birth_date,      presence: true
 end
