0d16b53c6e5ab71eaaa019889dd5f844b49c8e3e
F-tomo <tmk.determination714@icloud.com>
2020-12-20 15:45:34 +0900

    conditionテーブル(ActiveHash)の作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
   belongs_to :user
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :discription
-    validates :category_id, numericality: { other_than: 1 }
+    validates :price, inclusion: {in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
     validates :condition_id
     validates :postage_id
     validates :prefecture_id
     validates :hold_date_id
-    validates :price, inclusion: {in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
   end
 end
