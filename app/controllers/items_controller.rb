ee5eee912bbace52abef5241af32c3084a0219cc
F-tomo <tmk.determination714@icloud.com>
2020-12-23 22:24:35 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,17 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
   
   private
