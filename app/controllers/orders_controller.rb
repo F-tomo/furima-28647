0eda5adefaeb98aca585ccdfcc584ed7c07beb77
F-tomo <tmk.determination714@icloud.com>
2021-01-18 22:43:06 +0900

    orderコントローラーの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,44 @@
 class OrdersController < ApplicationController
+
+  before_action :set_item, only: [:index, :create]
+
   def index
+    @item_order = ItemOrder.new
+  end
+
+  def create
+    @item_order = ItemOrder.new(order_params)
+     binding.pry
+    if @item_order.valid?
+      pay_item
+      @item_order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(
+      :post_code, :prefecture_id, :city, :house_number, :building, :phone, :item_id, :token
+      ).merge(
+      user_id: current_user.id
+      )
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_29573ea5ca44d57d6caabdb8"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
+
 end
