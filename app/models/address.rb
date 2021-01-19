84c219685f83215ec48b146684ad1170c6a97bce
F-tomo <tmk.determination714@icloud.com>
2021-01-19 21:48:39 +0900

    addressモデルの不要なコードの削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,16 +2,4 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :order
-
-  with_options presence: true, numericality: { other_than: 1 } do
-    validates :prefecture_id
-  end
-
-  with_options presence: true do
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :house_number
-    validates :phone, format: { with: /\A[0-9]+\z/ }
-    validates :order_id
-  end
 end
